a7ff03d0b31ddae6417d0249128f913e0c6738eb
parksence <parksence95@gmail.com>
2024-07-17 14:21:00 +0900

    SwaggerConfig 추가(스웨거 UI 설정)

---
+++ src/main/java/me/parkhuijun/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package me.parkhuijun.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Sample API Overview")
+                .version("V1")
+                .description("Detailed API documentation for developers.");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .info(info)
+                .security(Arrays.asList(securityRequirement));
+    }
+}
