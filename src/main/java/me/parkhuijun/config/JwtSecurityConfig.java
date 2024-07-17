ffaf2b33d6fe2ebde19af5861ff1472918d1b615
parksence <parksence95@gmail.com>
2024-07-17 14:20:42 +0900

    jwt 설정 추가

---
+++ src/main/java/me/parkhuijun/config/JwtSecurityConfig.java
@@ -0,0 +1,22 @@
+package me.parkhuijun.config;
+
+import lombok.RequiredArgsConstructor;
+import me.parkhuijun.auth.JwtFilter;
+import me.parkhuijun.auth.TokenProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+            new JwtFilter(tokenProvider),
+            UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
