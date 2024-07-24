c0f1271fe5f0343d4550b2372f5d4b33dfeacd9e
parksence <parksence95@gmail.com>
2024-07-24 23:50:03 +0900

    Constant.java

--- src/main/java/me/parkhuijun/commons/Constant.java
+++ src/main/java/me/parkhuijun/commons/Constant.java
@@ -9,16 +9,4 @@ public class Constant {
         public static final int MALFORMED = 400;
         public static final int EXCEPTION = 500;
     }
-
-//    public static class KafkaEvent {
-//        public static final String DATA_CHECK_COMPLETE = "DataCheckComplete";
-//        public static final String DATA_CHECK_WARNING = "DataCheckWarning";
-//        public static final String DATA_CHECK_ERROR = "DataCheckError";
-//        public static final String TRAINING = "Training";
-//        public static final String TRAINING_COMPLETE = "TrainingComplete";
-//        public static final String FILE_UPLOAD_COMPLETE = "FileUploadComplete";
-//        public static final String TABULAR_DATASET_UPLOAD_COMPLETE = "TabularDatasetUploadComplete";
-//        public static final String TABULAR_MODEL_TRAINING = "TabularModelTraining";
-//        public static final String TABULAR_MODEL_TRAINING_COMPLETE = "TabularModelTrainingComplete";
-//    }
 }
